c33aae44432b56342028db1e15eae9e8c8858f0d
Roy <J.jyun0525@gmail.com>
2022-02-20 18:54:17 +0900

    방 목록 어댑터 뼈대 작업

---
+++ app/src/main/java/com/example/landapp_yun/adapters/RoomAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.landapp_yun.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.landapp_yun.R
+import com.example.landapp_yun.datas.Room
+
+// 주생성자에게서 필요한 재료를 받자. : 주 생성자에게서 받은 재료를 순서대로 부모에게 넘기자.
+
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<Room>) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+        val inflater = LayoutInflater.from(mContext)
+
+//    getView함수를 우리가 원하는 모양대로 뿌려지게 하려고 override함.
+//    gerView함수를
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.room_list_item,null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+}
